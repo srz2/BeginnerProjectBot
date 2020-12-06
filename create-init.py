84216ad2c3758daec18646cb87b70049c7fab087
srz2 <srz222@gmail.com>
2020-12-06 03:44:12 -0500

    Created helper script to create the praw.ini

---
+++ create-init.py
@@ -0,0 +1,32 @@
+# create-ini
+#
+# This will read and create the required INI file for this bot based on enviornment variables
+#
+# Usage: python3 create-ini.py <--force>
+#   --force: Optionally force create
+#
+
+import os
+import configparser
+
+file_example_ini = 'src/praw.ini.example'
+file_output_ini = 'src/praw.ini'
+
+if not os.path.exists(file_example_ini):
+    print('Example INI file not found')
+    exit(1)
+
+config = configparser.ConfigParser()
+config.read(file_example_ini)
+
+def get_env(key):
+    return os.environ.get(key)
+
+config['DEFAULT']['CLIENT_ID'] = get_env('CLIENT_ID')
+config['DEFAULT']['CLIENT_SECRET'] = get_env('CLIENT_SECRET')
+config['DEFAULT']['USER_AGENT'] = get_env('USER_AGENT')
+config['DEFAULT']['USERNAME'] = get_env('USERNAME')
+config['DEFAULT']['PASSWORD'] = get_env('PASSWORD')
+
+with open(file_output_ini, 'w') as config_file:
+    config.write(config_file, space_around_delimiters=False)
