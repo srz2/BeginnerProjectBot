61af78a60925e004c2cf27bbc4076b0270205a5c
posiaden123 <dewest88@gmail.com>
2021-02-06 21:27:41 -0500

    fixed imports

--- test/unit-tests/test_utilities.py
+++ test/unit-tests/test_utilities.py
@@ -1,5 +1,7 @@
 import unittest
-from projectbot.Utilities import is_recongized_difficulty, create_link_reference, check_file_exists
+from projectbot.Utilities import is_recongized_difficulty
+from projectbot.Utilities import create_link_reference
+from projectbot.Utilities import check_file_exists
 
 class ReconigzedDifficulty(unittest.TestCase):
     def test_recnogized_difficulty_all(self):
